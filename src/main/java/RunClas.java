e15f347eb9617c3debd16c9d1f5758a048cf6fd8
syuvkovetska <syuvkovetska@playtika.com>
2019-12-13 14:56:29 +0200

    edit user info + add reading from file

--- src/main/java/RunClas.java
+++ src/main/java/RunClas.java
@@ -6,25 +6,18 @@ import javax.mail.MessagingException;
 import com.opencsv.exceptions.CsvException;
 import utils.CsvHelper;
 import utils.EmailHelper;
+import utils.FileRider;
 import utils.Santa;
 import utils.SantaBill;
 import utils.User;
 
 public class RunClas {
-    private static String textTemplate = "<h1 style=\"text-align: center;\"><img src=\"https://encrypted-tbn0.gstatic" +
-            ".com/images?q=tbn:ANd9GcSFm8fVaYWzBdRUG-eym-X3OzKByMQZD_a6zifPXZ8ffAJwvZIV_Q&amp;s\" alt=\"\" width=\"379\" height=\"133\" /></h1>\n" +
-            "<h1 style=\"text-align: center;\">Secret Santa Gift Exchange</h1>\n" +
-            "<h3 style=\"text-align: center;\">Hi SantaUser, You will be Secret Santa for <strong>User</strong></h3>\n" +
-            "<p style=\"text-align: center;\"><strong>Gifts should cost 150-200UAN</strong></p>\n" +
-            "<h4 style=\"text-align: center;\"><strong>Best wishes for a Merry Christmas and a Happy New Year!</strong></h4>\n" +
-            "<p style=\"text-align: center;\"><strong><img src=\"https://encrypted-tbn0.gstatic" +
-            ".com/images?q=tbn:ANd9GcQ3_ueuAIdqRzwKMaCfnrAKD5iRBH7UaKJcBEFxWPK4m-bggKmX6A&amp;s\" alt=\"\" width=\"451\" height=\"112\" /></strong></p>\n" +
-            "<h3>&nbsp;</h3>";
-
 
     private static String path = "C:\\Users\\syuvkovetska\\Desktop\\autoTests\\mini-project\\src\\main\\resources\\users.csv";
+    private static String htmlPath = "C:\\Users\\syuvkovetska\\Desktop\\autoTests\\SantaProject\\src\\main\\resources\\secretSantaTemplate";
 
     public static void main(String[] args) throws IOException, CsvException, MessagingException {
+        String textTemplate = FileRider.getFile(htmlPath);
 
         User user = new User();
         SantaBill santaBill = new SantaBill();
@@ -38,6 +31,7 @@ public class RunClas {
             String personalTemplate = textTemplate.replace("SantaUser", happySanta.getSanta().getName());
             personalTemplate = personalTemplate.replace("User", happySanta.getSantaTo().getName());
            // emailHelper.sendEmail("svitlana.slobodyan894@gmail.com", "svitlanas1", "Secret Santa", personalTemplate, happySanta.getSanta().getEmail());
+            emailHelper.sendEmail("svitlana.slobodyan894@gmail.com", "svitlanas1", "Secret Santa", personalTemplate, "feridesviti@gmail.com");
             System.out.println("Sended for " + happySanta.getSanta().getName());
 
         }
--- src/main/java/utils/FileRider.java
+++ src/main/java/utils/FileRider.java
@@ -1,4 +1,22 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileRider {
+
+    public static String getFile(String fileName) throws IOException {
+        String fullFile = "";
+        File file = new File(fileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        String line;
+        while((line = br.readLine()) != null){
+            fullFile = fullFile + line;
+        }
+        return fullFile;
+    }
 }
